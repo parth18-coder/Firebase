827d12349046deadfad65e721b3cf098170f8763
Parth <banparth57@gmail.com>
2023-07-22 00:47:59 +0530

    Integrating the firebase with the Android Studio

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+    }
+}
